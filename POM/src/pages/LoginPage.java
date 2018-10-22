538b31482155dc68f1bc11a17edb4339a0c09aa2
Balanarasimha <Balanarasimha@vijayalaxmi>
2018-10-22 22:51:30 +0530

    cmmit

--- POM/src/pages/LoginPage.java
+++ POM/src/pages/LoginPage.java
@@ -1,6 +1,40 @@
 package pages;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.hssf.record.formula.functions.Row;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.formula.functions.Column;
+import org.apache.poi.ss.usermodel.Cell;
+
 public class LoginPage {
 
-	
+	HSSFWorkbook workbook;
+	HSSFSheet sheet;
+	HSSFRow hssfRow;
+	HSSFCell hssfCell;
+	FileInputStream inputStream;
+	Row row;
+	Column column;
+	Cell cell;
+
+	public void setFile(String filePath) {
+		try {
+			inputStream = new FileInputStream(filePath);
+			workbook = new HSSFWorkbook(inputStream);
+			sheet = workbook.getSheet("Sheet1");
+			int k = sheet.getLastRowNum();
+			System.out.println(k);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
 }
--- POM/src/pages/Two.java
+++ POM/src/pages/Two.java
@@ -2,4 +2,8 @@ package pages;
 
 public class Two {
 
+	public static void main(String[] args) {
+		LoginPage page = new LoginPage();
+		page.setFile("C:\\Users\\Balanarasimha\\Desktop\\Book.xls");
+	}
 }
